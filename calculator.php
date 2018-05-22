1048f490479f6c13e0cbcc81e0bde1c27315af29
Lindsay <lindsay.wang@lessannoyingcrm.com>
2018-05-22 13:58:30 -0500

    Both PHP Calculators
    One has a drop down of 4 functions, and the other mixes up the two colors you put in and changes the screen to the result!

--- calculator.php
+++ calculator.php
@@ -1,17 +1,19 @@
-<?php
 
-
-function Add($variable1, $variable2){
-		return $variable1 + $variable2;
-}
-
-echo "
+<html>
 	<form action=''>
 		<input type='text' name='variable1' />
 		<input type='text' name='variable2'	/>
 		<input type= 'submit' />
 	</form>
-";
+</html>
+
+
+<?php
+
+function Add($variable1, $variable2){
+		return $variable1 + $variable2;
+}
+
 
 if(isset($_REQUEST['variable1'])) {
 	echo "<h1>The answer is: ".Add($_REQUEST['variable1'], $_REQUEST['variable2'])."</h1>";
