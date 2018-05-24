71d504d4c87b48e8735b29e23332ec047a57b822
Lindsay <lindsay.wang@lessannoyingcrm.com>
2018-05-24 15:49:46 -0500

    PHP organized blog
    The third PHP challenge

---
+++ PHPblog.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<title>Lindsay's Blog</title>
+		<link rel='stylesheet' href='maindesign.css' />
+		<link href="https://fonts.googleapis.com/css?family=Lobster|Montserrat" rel="stylesheet">
+	</head>
+
+	<body>
+		<h1 class='header'> Lindsay's Blog </h1>
+		<h2 class='subtitle'>WORDS FROM A GIRL LEARNING HOW TO CODE </h1>
+		<div class='blogpost'; style=background-color:#bc1a1a;>
+			<h3>Halfway Done with College </h3>
+			<p class='white'>A look back on sophomore year</p>
+			<p class='white'><a href='http://localhost:8888/view_post.php?postId=0'>Read on</a></p>
+		</div>
+
+		<div class='blogpost'; style=background-color:#153466;>
+			<h3>My Summer in St. Louis</h3>
+			<p class='white'> Code, run, sleep.</p>
+			<p class='white'><a href='http://localhost:8888/view_post.php?postId=1'>Read on</a></p>
+		</div>
+
+		<div class='blogpost'; style=background-color:#8fd7e8;>
+			<h3>My opinion on carrots</h3>
+			<p class='white'> The inside scoop on this special veggie</p>
+			<p class='white'><a href='http://localhost:8888/view_post.php?postId=2'>Read on</a></p>
+		</div>
+
+
+
+
+
+</body>
+</html>
