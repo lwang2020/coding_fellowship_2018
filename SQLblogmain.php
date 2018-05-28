47a5ceb9acb96742472dd80c7b326198db225de5
Lindsay <lindsay.wang@lessannoyingcrm.com>
2018-05-28 15:18:16 -0500

    MySQL

---
+++ SQLblogmain.php
@@ -0,0 +1,36 @@
+<?php
+include ('/Applications/MAMP/htdocs/include/SQLProject.php');
+?>
+
+
+<html>
+
+	<head>
+		<title>Lindsay's Blog</title>
+		<link rel='stylesheet' href='maindesign.css' />
+		<link href="https://fonts.googleapis.com/css?family=Lobster|Montserrat" rel="stylesheet">
+	</head>
+
+	<body>
+		<h1 class='header'> Lindsay's Blog </h1>
+		<h2 class='subtitle'>WORDS FROM A GIRL LEARNING HOW TO CODE </h1>
+
+<?php
+
+function PostABlock($blocknumber){
+	$sorter=getSorter();
+	$item = $sorter[$blocknumber];
+		echo "<div class='blogpost'; style='background-color:$item[BlockColor]'>";
+		echo "<h3>$item[ArticleTitle]</h3>";
+		echo "<p class='white'>$item[Subtitle]</p>";
+		echo "<p class='white'><a href=$item[ReadOn]>Read on</a></p>";
+}
+
+PostABlock('3');
+PostABlock('4');
+PostABlock('5');
+
+?>
+
+	</body>
+</html>
