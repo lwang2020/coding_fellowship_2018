8a3131d5290db8ba8f77d098aaeeee43904ed9bf
Lindsay <lindsay.wang@lessannoyingcrm.com>
2018-06-27 17:06:15 -0500

    laptop tested

---
+++ ResumeStage.php
@@ -0,0 +1,11 @@
+<?php
+ 
+include ('include/ResumeInclude.php');
+
+
+$DisplayPage = DisplayPage();
+
+echo "<h1 class='PageTitle'>".$DisplayPage[$_REQUEST['page']]['Title']."</h1>";
+echo "<p class= 'PageSubtitle'>".$DisplayPage[$_REQUEST['page']]['SubTitle']."</p>";
+echo "<div class='NavBar'>".$DisplayPage[$_REQUEST['page']]['NavigationBar']."</div>";
+echo $DisplayPage[$_REQUEST['page']]['Content'];
