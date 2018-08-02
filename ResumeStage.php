c6d70f2e45e0320e3c578168001f6b2fb0b2bc83
Lindsay <lindsay.wang@lessannoyingcrm.com>
2018-08-02 10:35:42 -0500

    trying again :(

--- ResumeStage.php
+++ ResumeStage.php
@@ -1,4 +1,4 @@
-
+ 
 <?php
 include ('include/ResumeInclude.php');
 $DisplayPage = DisplayPage();
--- include/ResumeInclude.php
+++ include/ResumeInclude.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 include ('include/ResumeBackstage.php');
 echo "<link rel='stylesheet' href='ResumeDesign.css' />";
 echo "<link href='https://fonts.googleapis.com/css?family=Raleway|Oswald:300|Lato:300,400' rel='stylesheet'>";
